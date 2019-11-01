4334c9a726da134ab7604a0d730c05607d310e86
Cihan Biyikoglu <cbiyikoglu@caml113970.rms.com>
2019-11-01 07:35:52 -0700

    more conditional on plugging order

--- rotate.sh
+++ rotate.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-./fb-rotate -d 0x1b56b4a3 -r 90
-./fb-rotate -d 0x1b56b4a8 -r 270
+command="./fb-rotate -i | grep 0x1b56b4a4"
+orientation=$(eval $command)
+if [[ $orientation != "" ]]
+then
+    ./fb-rotate -d 0x1b56b4a4 -r 90
+    ./fb-rotate -d 0x1b56b4a7 -r 270
+else
+    ./fb-rotate -d 0x1b56b4a3 -r 90
+    ./fb-rotate -d 0x1b56b4a8 -r 270
+fi
